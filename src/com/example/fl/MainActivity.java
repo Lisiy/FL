70004a5a97de0720a350fd3399e1a97a0c24fc2d
Lisiy <lisomu@gmail.com>
2014-06-30 20:11:45 +0300

    add log, state

--- src/com/example/fl/MainActivity.java
+++ src/com/example/fl/MainActivity.java
@@ -1,28 +1,23 @@
 package com.example.fl;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ToggleButton;
+
 
 public class MainActivity extends ActionBarActivity {
 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -33,33 +28,29 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    
+    public void ToggleFlash(View v)
+    {
+    	Camera cam = Camera.open();
+        if (cam == null) return;
+        Parameters p = cam.getParameters();
+        
+    	ToggleButton button = (ToggleButton)findViewById(R.id.FlashToggle);
+    	if (button.isChecked())
+    	{
+    		Log.i("FL", "Button is checked, flashlight on");
+    		p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+    	}
+    	else 
+    	{
+    		Log.i("FL", "Button is not checked, flashlight off");
+    		p.setFlashMode(Parameters.FLASH_MODE_OFF);
+    	}
+     
+    	cam.setParameters(p);
+    	cam.startPreview();
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
